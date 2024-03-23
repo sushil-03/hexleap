aef644d34e20293549754e3c0fba77130cacaad8
Sushil <sushilrawat1720@gmail.com>
2024-03-23 19:36:05 +0530

    margin

--- src/components/Sports.tsx
+++ src/components/Sports.tsx
@@ -4,12 +4,10 @@ import SportCard from "./SportCard";
 import { SportCardType } from "@/types.dt";
 const Sports = () => {
   return (
-    <React.Fragment>
-      <div>
-        <span className=" text-2xl font-semibold pb-1 border-b-2 border-[#738FFF] font-popps ml-2">
-          Sports
-        </span>
-      </div>
+    <div>
+      <span className=" text-2xl font-semibold pb-1 border-b-2 border-[#738FFF] font-popps ml-2">
+        Sports
+      </span>
       <div className="flex flex-col gap-5 items-center">
         <div className="flex gap-5 mt-5 justify-between overflow-x-scroll py-4 w-full">
           {sports.map((sport: SportCardType) => {
@@ -22,7 +20,7 @@ const Sports = () => {
           </button>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
